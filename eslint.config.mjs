eb30b143229647587740a876c480dca27cc07d74
Tobias Klumpp <tobias.klumpp@toklumpp.net>
2026-09-26 17:10:42 +0100

    Add typescript checking

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,16 @@
 Copyright (c) 2026 Tobias Klumpp (https://www.toklumpp.net/)
 SPDX-License-Identifier: MIT
 */
+// @ts-check
 import js from "@eslint/js";
 import globals from "globals";
 
+/** @type {import("eslint").Linter.Config[]} */
 export default [
   js.configs.recommended,
   {
     languageOptions: {
+      /** @type {import("eslint").Linter.LanguageOptions["globals"]} */
       globals: {
         ...globals.browser,
       },
